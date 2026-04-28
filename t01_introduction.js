d994cc784ea786dea5c5e3e5448b1e75956e8ecf
Fat Naoya <24345jh@hvhs.school.nz>
2026-04-28 23:59:45 +0000

    Apr 28, 2026, 11:59 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,9 +1,9 @@
-/****************************
+/*************************
 This is a block comment.
 Put your header comment here!
 ****************************/
 console.log("Running t01_introduction.js")
-console.log(/*Insert your message here!*/)
+console.log('')
 
-// This is a single line comment
+/********************** */
 
