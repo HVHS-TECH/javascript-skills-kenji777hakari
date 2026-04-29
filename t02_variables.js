5038aef2b6e68317cd20a2006e0cf1a638d311be
Fat Naoya <24345jh@hvhs.school.nz>
2026-04-29 00:04:47 +0000

    Apr 29, 2026, 12:04 AM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,8 @@
-/* Paste your code from task 1 here */
+/* Paste your code from task 1 here *//*************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t02_variables.js")
+
+/********************** */
+
