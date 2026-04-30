20d0965e0c3c284de26f925d63a779ec9234b39e
Fat Naoya <24345jh@hvhs.school.nz>
2026-04-30 01:12:37 +0000

    Apr 30, 2026, 1:12 AM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,20 @@
-c
+/* Paste your code from task 1 here *//*************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t02_variables.js")
+//variable
+
+var username = "Jayden";
+var userage = "15";
+var clothes = ["shirt", "pants", "shoes"];
+var pocketmoney = "3";
+
+/***********main code*********** */
+console.log(username);
+console.log(userage);
+console.log(clothes);
+console.log(pocketmoney);
+
+
+
