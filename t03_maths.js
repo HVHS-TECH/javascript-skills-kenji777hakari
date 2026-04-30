269b1febbfe0775ea869825610b3e370cd409a87
Fat Naoya <24345jh@hvhs.school.nz>
2026-04-30 01:11:31 +0000

    Apr 30, 2026, 1:11 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,19 @@
-/* Paste your code from task 2 here */
+/* Paste your code from task 2 here *//* Paste your code from task 1 here *//*************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t03_maths.js")
+//variable
+
+var username = "Jayden";
+var userage = "15"
+var clothes = ["shirt", "pants", "shoes"]
+
+/***********main code*********** */
+console.log(username)
+console.log(userage)
+console.log(clothes)
+console.log(pocketmoney)
+
+
+
