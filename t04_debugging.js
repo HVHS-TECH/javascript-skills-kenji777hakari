33e924d9cc7f5cbf39bc4ba9000534f625c4540b
Fat Naoya <24345jh@hvhs.school.nz>
2026-05-11 21:54:28 +0000

    May 11, 2026, 9:54 PM

--- t04_debugging.js
+++ t04_debugging.js
@@ -4,11 +4,11 @@ console.log("Task 4 - Debugging")
 var area = length + width;
 
 // calculate the perimeter
-var perimeter = 2  length + 2 * width; 
+var perimeter = 2  length + 2 * "width"; 
 
 // Set up the size of the page.
 var length = 8;
-var width = 10
+var width = 10;
 
 // Display the results
 console.log("A area of a piece of paper with a length of "+length+", and a width of "+width+" is "+ area)
