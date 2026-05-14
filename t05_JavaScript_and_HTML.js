2e64ddc44b5ba0f6cd7b412edda2a9d7bc19e872
Fat Naoya <24345jh@hvhs.school.nz>
2026-05-14 00:47:54 +0000

    May 14, 2026, 12:47 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,24 @@
-/* Paste your code from task 3, Maths here */
+/* Paste your code from task 1 here *//*************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t03_maths.js")
+//variable
+
+var username = "Jayden";
+var userage = "15";
+var clothes = ["shirt", "pants", "shoes"];
+var pocketmoney = "3";
+var year = "2026"
+var myNumber = "1";
+
+
+/***********main code*********** */
+console.log("hi " + username);
+console.log(userage);
+console.log(clothes);
+console.log(pocketmoney);
+console.log(1 + 2)
+
+
+
