4a58cab48f6bdf87388a3787b364c07f302c587a
Fat Naoya <24345jh@hvhs.school.nz>
2026-05-18 21:27:32 +0000

    May 18, 2026, 9:27 PM

--- t06_functions.js
+++ t06_functions.js
@@ -18,7 +18,8 @@ function writeline(){
     OUTPUT.innerHTML +="<p>Bubble Pop Electric! </p>"
 }
 
-wrtieline();{ }
+wrtieline();{
+    
 
 /***********main code*********** */
 console.log("hi " + username);
