35fad60dd79c8c5aa0114d743311a4d4cb7c5963
Fat Naoya <24345jh@hvhs.school.nz>
2026-05-18 21:43:27 +0000

    May 18, 2026, 9:43 PM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -1,1 +1,40 @@
-/* Paste your code from task 6 here */
+/* Paste your code from task 1 here *//*************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t06_fucntions.js")
+//variable
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+
+var username = "Jayden";
+var userage = "15";
+var clothes = ["shirt", "pants", "shoes"];
+var pocketmoney = "3";
+var year = "2026"
+var myNumber = "1";
+
+/***********functions*******************/
+
+function writeline(){
+    OUTPUT.innerHTML +="<p>Bubble Pop Electric! </p>"
+}
+
+writeline()
+    
+
+/***********main code*********** */
+console.log("hi " + username);
+console.log(userage);
+console.log(clothes);
+console.log(pocketmoney);
+console.log(1 + 2)
+
+
+
+
+
+OUTPUT.innerHTML += "<h2>HAPPY BRITHDAY DANIEL</h2>"
+writeline()
+/**********************************************************/
